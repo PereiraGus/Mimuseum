af5a9c85f401c651116b26f8ae8cd7b0fc1b1501
PereiraGus <gustavo.oito@hotmail.com>
2022-08-24 07:48:32 -0300

    Adição da API ao repositório

---
+++ API/API Mimuseum/API Mimuseum/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_Mimuseum.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ API/API Mimuseum/API Mimuseum/Models/Arte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_Mimuseum.Models
+{
+    public class Arte
+    {
+        public int IDAarte { get; set; }   
+        public string NomeArte { get; set; }    
+        public string NomeArtista { get; set; } 
+        public int AnoArte { get; set; }    
+        public string EstiloArte { get; set; } 
+        public string UrlArte { get; set; }
+        
+    }
+}
