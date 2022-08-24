19bd8386f00bd9c402970bdf8c86a1f609304c60
PereiraGus <gustavo.oito@hotmail.com>
2022-08-24 08:29:03 -0300

    Inserção de artes no git

--- API/API Mimuseum/API Mimuseum/Controllers/ArteController.cs
+++ API/API Mimuseum/API Mimuseum/Controllers/ArteController.cs
@@ -4,36 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using API_Mimuseum.Models;
 
 namespace API_Mimuseum.Controllers
 {
     public class ArteController : ApiController
     {
-        // GET: api/Arte
-        public IEnumerable<string> Get()
+        Arte[] artes = new Arte[]
         {
-            return new string[] { "value1", "value2" };
+            new Arte { IDAarte = 1, NomeArte = "A Grande Onda de Kanagawa", NomeArtista = "Katsushika Hokusai", AnoArte = 1831,
+                EstiloArte = "Gravura", UrlArte = "https://artsandculture.google.com/asset/the-great-wave-off-the-coast-of-kanagawa/fAFp7yddSAtcTQ?hl=pt-BR"
+            },
+            new Arte { IDAarte = 2, NomeArte = "Mona Lisa", NomeArtista = "Leonardo da Vinci", AnoArte = 1530,
+                EstiloArte = "Pintura a óleo", UrlArte = "https://cdn.pariscityvision.com/library/image/5449.jpg"
+            }
+        };
+        public IEnumerable<Arte> GetAllWorksOfArt() { return artes; }
+        public Arte GetArtById(int id)
+        {
+            var arte = artes.FirstOrDefault((p) => p.IDAarte == id);
+            if (arte == null)
+            {
+                var asw = new HttpResponseMessage(HttpStatusCode.NotFound);
+                throw new HttpResponseException(asw);
+            }
+            return arte;
         }
-
-        // GET: api/Arte/5
-        public string Get(int id)
+        public IEnumerable<Arte> GetArtsByName(string name)
         {
-            return "value";
-        }
-
-        // POST: api/Arte
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/Arte/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Arte/5
-        public void Delete(int id)
-        {
+            var res = artes.Where((p) => p.NomeArte.Contains(name));
+            return res;
         }
     }
 }
