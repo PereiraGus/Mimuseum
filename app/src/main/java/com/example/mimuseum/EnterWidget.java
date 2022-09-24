cd38ccef930048708f9872519ce9e55527e2c26d
BrnBastos <bruninhosbastos0228@gmail.com>
2022-09-23 21:34:29 -0300

    commit geo loc e put post.

---
+++ app/src/main/java/com/example/mimuseum/EnterWidget.java
@@ -0,0 +1,34 @@
+package com.example.mimuseum;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/** Implementation of App Widget functionality.*/
+
+public class EnterWidget extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, mainMenu.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.enter_widget);
+            views.setOnClickPendingIntent(R.id.appwidget_button, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
